4a8a1e610817675dcb3bf379b12144ffd1c52bc9
Juan Sequén <eduardsequen@gmail.com>
2024-08-06 17:52:21 -0600

    Funciones

---
+++ Pila.java
@@ -0,0 +1,10 @@
+import java.util.Stack;
+
+public class Pila{
+    public static void main (String [] args){
+
+        Stack<Integer> mi_stack = new Stack<>()
+
+        
+    }
+}
---
+++ funciones.py
@@ -0,0 +1,11 @@
+def saludar(name):
+    print("Hola", name)
+
+saludar("Alberto")
+
+#funcion que calcula la media de
+#3 números
+def media(x,y,z):
+    return (x+y+z)/3
+
+print("El resultado es:", media(78,45,98))
